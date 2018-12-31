29dce6fa4b7dc8e3444ec3c8f27aadc140136442
Saena <toastyhatesthesea@gmail.com>
2018-12-30 19:53:34 -0800

    Changed speed

--- Game1.cs
+++ Game1.cs
@@ -16,8 +16,12 @@ namespace AnimatedSprites
         Point pigFrameSize, pigCurrentFrame, pigSheetSize;
         Vector2 ringsPosition, skullPosition, pigposition;
         MouseState prevMouseState;
-        const float ringsSpeed = 6;
+        float ringsSpeed = 6;
+        float skullSpeed = 7;
+        float pigSpeed = 4;
         const int collisionRectOffset = 10;
+
+        private int timeSinceLastFrame, milliSecondsPerFrame;
 
         public Game1()
         {
@@ -26,6 +30,8 @@ namespace AnimatedSprites
             ringsPosition = new Vector2(0, 0);
             skullPosition = new Vector2(100, 100);
             pigposition = new Vector2(Window.ClientBounds.Width / 2, Window.ClientBounds.Height / 2);
+            timeSinceLastFrame = 0;
+            milliSecondsPerFrame = 500;
 
             //TargetElapsedTime = new System.TimeSpan(0, 0, 0, 0, 16);
           
@@ -76,7 +82,7 @@ namespace AnimatedSprites
             pig = Content.Load<Texture2D>("Images/guineapigs");
             pigCurrentFrame = new Point(0, 0);
             pigFrameSize = new Point(pig.Width / 12, pig.Height / 8);
-            pigSheetSize = new Point(3, 3);
+            pigSheetSize = new Point(3, 0);
         }
 
         /// <summary>
@@ -120,26 +126,62 @@ namespace AnimatedSprites
                 }
             }
 
+            timeSinceLastFrame = timeSinceLastFrame + gameTime.ElapsedGameTime.Milliseconds;
+            if (timeSinceLastFrame > milliSecondsPerFrame)
+            {
+                timeSinceLastFrame = timeSinceLastFrame - milliSecondsPerFrame;
+                pigCurrentFrame.X++;
+                if (pigCurrentFrame.X >= pigSheetSize.X)
+                {
+                    pigCurrentFrame.X = 0;
+                    pigCurrentFrame.Y++;
+                    if (pigCurrentFrame.Y >= pigSheetSize.Y)
+                    {
+                        pigCurrentFrame.Y = 0;
+                    }
+                }
+            }
+
+            skullPosition.X = skullPosition.X + skullSpeed;
+            if (skullPosition.X > Window.ClientBounds.Width)
+            {
+                skullSpeed = skullSpeed * -1;
+            }
+            else if (skullPosition.X < 0)
+            {
+                skullSpeed = skullSpeed * -1;
+            }
+
+            ringsPosition.X = ringsPosition.X + ringsSpeed;
+            if(ringsPosition.X > Window.ClientBounds.Width)
+            {
+                ringsSpeed = ringsSpeed * -1;
+            }
+            else if(ringsPosition.X < 0)
+            {
+                ringsSpeed = ringsSpeed * -1;
+            }
+
             KeyboardState keyboardState = Keyboard.GetState();
 
-            if (ringsPosition.X + ringFrameSize.X / 2 < Window.ClientBounds.Width)
+            if (pigposition.X + pigFrameSize.X / 2 < Window.ClientBounds.Width)
             {
                 if (keyboardState.IsKeyDown(Keys.Right))
-                    ringsPosition.X += ringsSpeed;
+                    pigposition.X += pigSpeed;
             }
             if (keyboardState.IsKeyDown(Keys.Left))
-                ringsPosition.X -= ringsSpeed;
+                pigposition.X -= pigSpeed;
 
             if (keyboardState.IsKeyDown(Keys.Up))
-                ringsPosition.Y -= ringsSpeed;
+                pigposition.Y -= pigSpeed;
             if (keyboardState.IsKeyDown(Keys.Down))
-                ringsPosition.Y += ringsSpeed;
+                pigposition.Y += pigSpeed;
 
-            MouseState mouseState = Mouse.GetState();
+            /*MouseState mouseState = Mouse.GetState();
             if (mouseState.X != prevMouseState.X ||
                 mouseState.Y != prevMouseState.Y)
                 ringsPosition = new Vector2(mouseState.X, mouseState.Y);
-            prevMouseState = mouseState;
+            prevMouseState = mouseState;*/
 
             if (Collide())
             {
@@ -175,6 +217,14 @@ namespace AnimatedSprites
                     skullFrameSize.Y),
                 Color.White, 0, Vector2.Zero,
                 1, SpriteEffects.None, 0);
+            spriteBatch.Draw(pig, pigposition,
+                 new Rectangle(pigCurrentFrame.X * pigFrameSize.X,
+                     pigCurrentFrame.Y * pigFrameSize.Y,
+                     pigFrameSize.X,
+                     pigFrameSize.Y),
+                 Color.White, 0, Vector2.Zero,
+                 3, SpriteEffects.None, 0);
+
             spriteBatch.End();
 
             base.Draw(gameTime);
